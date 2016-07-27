9fb66c72c2fb201531abdd473ad810bce239e8f3
William Park <williampark@hotmail.co.uk>
2016-07-27 20:52:34 +0100

    V0.2.0
    - Loads more flexibility to change the outputs and how they're saved.
    - Improved "default" options (nicer outputs for more websites)

--- peepjs.js
+++ peepjs.js
@@ -4,14 +4,32 @@
 
 var Nightmare = require('nightmare');
 var sharp = require('sharp');
+var fs = require('fs');
 
 module.exports = class {
   //Constructor Method
-  constructor() {
+  constructor(options) {
     console.log('[PEEPJS] STARTED');
+
     this.nightmare = Nightmare({
-      show: false
+      show: options.show || false,
+      width: options.width || 800,
+      height: options.height || 600,
+      useContentSize: options.useContentSize || false,
+      enableLargerThanScreen: options.enableLargerThanScreen || false,
+      webPreferences: {
+        zoomFactor: options.zoomFactor || 0.75,
+        images: options.showImages === false ? false : true
+      }
     });
+
+    this.showScrollbars = options.showScrollbars || false;
+    this.centered = options.centered || false;
+    this.thumbnailOnly = options.thumbnailOnly || false;
+    this.fullsizeOnly = options.fullsizeOnly || false;
+
+    this.fullsizePrefix = options.fullsizePrefix || "full_";
+    this.thumbnailPrefix = options.thumbnailPrefix || "thumb_";
 
     this.url = null;
     this.directory = null;
@@ -35,29 +53,45 @@ module.exports = class {
       this.ftype = type;
       this.fname = splitUrl();
 
-      let output = `${directory}full_${this.fname}${type}`;
+      let output = `${this.directory}${this.fullsizePrefix}${this.fname}${this.ftype}`;
       console.log(`[PEEPJS] TRYING: ${output}`);
 
       //Use nightmare instance to attempt screenshot on url.
       this.nightmare
         .goto(url)
-        .evaluate(() => {
-          //Hides scrollbars, and scrolls to horizontal center of the page.
-          document.documentElement.style.overflow = 'hidden';
-          document.body.scroll = "no";
-          //document.body.scrollLeft = (document.body.scrollWidth - document.body.clientWidth) / 2
+        .evaluate((clientOptions) => {
+          //Hides scrollbars
+          if(!clientOptions.showScrollbars) {
+            document.documentElement.style.overflow = 'hidden';
+            document.body.scroll = "no";
+          }
+          //Hides scrolls to horizontal center of the page.
+          if(clientOptions.centered) {
+            document.body.scrollLeft = (document.body.scrollWidth - document.body.clientWidth) / 2
+          }
+        }, {
+          //Pass these varaibles into the evaluate client
+          showScrollbars: this.showScrollbars,
+          centered: this.centered
         })
         .screenshot(output)
         .end()
         .then(() => {
           console.log(`[PEEPJS] SUCCESS: ${output}`);
           this.screenshot.fullsize = output;
-          this.createThumbnail().then(() => {
+
+          //Create the thumbnail
+          if(!this.fullsizeOnly) {
+            this.createThumbnail().then(() => {
+              resolve(this.screenshot);
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+          } else {
             resolve(this.screenshot);
-          })
-          .catch((err) => {
-            console.error(err);
-          });
+          }
+
         })
         .catch(function(err) {
           console.error(err);
@@ -70,8 +104,8 @@ module.exports = class {
   createThumbnail() {
     return new Promise((resolve, reject) => {
       //Set Up
-      let input = `${this.directory}full_${this.fname}${this.ftype}`;
-      let output = `${this.directory}thumb_${this.fname}${this.ftype}`;
+      let input = `${this.directory}${this.fullsizePrefix}${this.fname}${this.ftype}`;
+      let output = `${this.directory}${this.thumbnailPrefix}${this.fname}${this.ftype}`;
       console.log(`[PEEPJS] TRYING: ${output}`);
 
       //Use sharp to resize file.
@@ -85,6 +119,12 @@ module.exports = class {
         console.log(`[PEEPJS] SUCCESS: ${output}`);
         this.screenshot.thumb = output;
         resolve();
+
+        //If only the thumbnail is wanted, delete the fullsize.
+        if(this.thumbnailOnly) {
+          fs.unlink(input);
+          this.screenshot.fullsize = null;
+        }
       });
     });
   }
